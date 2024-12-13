2b77b3cd389843258197616fca04e63fe9498b89
Kassia Fraga <kassiafraga7@gmail.com>
2024-12-13 12:33:23 -0300

    Initial commit

---
+++ mobile/src/app/_layout.tsx
@@ -0,0 +1,27 @@
+import {Stack} from "expo-router";
+import {colors} from "@/styles/theme";
+import {Rubik_400Regular, Rubik_500Medium, Rubik_600SemiBold, Rubik_700Bold, useFonts} from "@expo-google-fonts/rubik";
+import {Loading} from "@/components/loading";
+import {GestureHandlerRootView} from "react-native-gesture-handler"
+
+
+export default function Layout() {
+    const [fontsLoaded] = useFonts({
+        Rubik_400Regular, Rubik_600SemiBold, Rubik_700Bold, Rubik_500Medium
+    })
+
+    if (!fontsLoaded) {
+        return <Loading/>
+    }
+
+    return (
+        <GestureHandlerRootView style={{flex: 1}}>
+            <Stack
+                screenOptions={{
+                    headerShown: false,
+                    contentStyle: {backgroundColor: colors.gray[100]},
+                }}
+            />
+        </GestureHandlerRootView>
+    )
+}
